9ef24d03ff197c11860d1b5aed4eee310510f4bf
evolyb <neu.nganhang@gmail.com>
2020-07-08 21:11:55 +0700

    add

--- Lesson 13/Tic-tac-toe/Tick-tac-toe.js
+++ Lesson 13/Tic-tac-toe/Tick-tac-toe.js
@@ -1,103 +1,196 @@
-let now = 1;
-let nut = "X"
-let arr = [["","",""],["","",""],["","",""]];
-let rwin = "";
-let cwin = "";
-let crosswin1 = "";
-let crosswin2 ="";
-let whichwin = "";
-function Add(r,c,nutnow) {
-    let ID = "X"+r+c;
-    document.getElementById(ID).value = nutnow;
-    arr[r-1][c-1] = nutnow;
-}
-function AddRand(r,c) {
-    if (arr[r-1][c-1] === ""){
-        if (now === 1){
-            nut = "X";
-            now = 0;
-        } else {
-            nut = 'O'
-            now = 1;
-        }
-        Add(r,c,nut);
-        if (CheckMate(r-1,c-1)){
-            document.getElementById("output").innerHTML = whichwin + " WIN";
-            FillColor();
-        }
-    } else {
-        alert("Ô này đã được chọn");
-    }
-}
-function CheckMate(r,c) {
-    if (arr[r][0] === arr[r][1] && arr[r][0] === arr[r][2]){
-        rwin = r;
-        whichwin = arr[r][0];
-        return true;
-    }
-    if (arr[0][c] === arr[1][c] && arr[0][c] === arr[2][c]){
-        whichwin = arr[0][c];
-        cwin = c;
-        return true;
-    }
-    if (arr[1][1] !== ""){
-        if (arr[0][0] === arr[1][1] && arr[0][0] === arr[2][2]) {
-            crosswin1 = 1;
-            whichwin = arr[1][1];
-            return true;
-        }
-        if (arr[0][2] === arr[1][1] && arr[0][2] === arr[2][0]) {
-            crosswin2 = 1;
-            whichwin = arr[1][1];
-            return true;
-        }
-    }
-    return false;
+let tableArray = [['','',''],['','',''],['','','']];
+let mark = 'X'
+let winPlayer = "";
+let winID = "";
+let howWin = "";
+let isOver =false;
+let countStep = 0;
+let gameType = "1";
+let rPossibleWin = -1;
+let cPossibleWin = -1;
+let rPossibleLose = -1;
+let cPossibleLose = -1;
+
+function selectGameType() {
+    gameType = document.getElementById("gameType").value;
 }
 
-function Reset() {
-    arr = [["","",""],["","",""],["","",""]];
-    now = 1;
-    nut = "X"
-    arr = [["","",""],["","",""],["","",""]];
-    rwin = "";
-    cwin = "";
-    crosswin1 = "";
-    crosswin2 ="";
-    whichwin = "";
-    for (let i = 1; i <= 3; i++) {
-        for (let j = 1; j <= 3; j++) {
-            let ID = 'X'+i+j;
-            document.getElementById(ID).value = "";
+function DisplayTable() {
+    for (let i = 0; i <=2 ; i++) {
+        for (let j = 0; j <= 2 ; j++) {
+            let ID = "X"+i+j;
+            document.getElementById(ID).innerHTML = tableArray[i][j];
+            document.getElementById(ID).style.backgroundColor = defaultStatus
+            isOver = false;
+            countStep = 0;
+            document.getElementById("output").innerHTML = "";
+            resetPossible();
         }
     }
-    document.getElementById("output").innerHTML = "";
-    for (let i = 1; i <= 3; i++) {
-        for (let j = 1; j <= 3; j++) {
-            let ID = 'Y'+i+j;
-            document.getElementById(ID).style.backgroundColor = "white";
+}
+function ClearTable() {
+    tableArray = [['','',''],['','',''],['','','']];
+    mark = "X";
+    DisplayTable();
+}
+function inputPlayer(r,c) {
+    if (checkInputAlready(r,c)) return;
+    if (isOver) return;
+    let ID = 'X'+r+c;
+    countStep++;
+    if (mark === 'X'){
+        document.getElementById(ID).innerHTML = 'X';
+        tableArray[r][c] = 'X';
+        mark = 'O';
+    } else {
+        document.getElementById(ID).innerHTML = 'O';
+        tableArray[r][c] = 'O';
+        mark = 'X';
+    }
+    if (checkWinner(r,c)){
+        document.getElementById("output").innerHTML = winPlayer + " is winner";
+        changeColor();
+        isOver = true;
+    }
+}
+function checkInputAlready(r,c) {
+    return tableArray[r][c] !== "";
+}
+function checkWinner(r,c) {
+    winPlayer = tableArray[r][c];
+    if (tableArray[r][0] === tableArray[r][1] && tableArray[r][0] === tableArray[r][2]) {
+        winID = r;
+        howWin = "row";
+        return true;
+    }
+    if (tableArray[0][c] === tableArray[1][c] && tableArray[0][c] === tableArray[2][c]){
+        winID = c;
+        howWin = "col";
+        return true;
+    }
+    if (tableArray[1][1] === "") return false;
+    if (tableArray[0][0] === tableArray[1][1] && tableArray[0][0] === tableArray[2][2]){
+        howWin = "1";
+        return true;
+    }
+    howWin ="2";
+    return tableArray[0][2] === tableArray[1][1] && tableArray[0][2] === tableArray[2][0];
+}
+function changeColor() {
+    switch (howWin) {
+        case "row":
+            for (let i = 0; i <=2 ; i++) {
+                let ID = "X"+winID+i;
+                document.getElementById(ID).style.backgroundColor="green";
+            }
+            break;
+        case "col":
+            for (let i = 0; i <=2 ; i++) {
+                let ID = "X"+i+winID;
+                document.getElementById(ID).style.backgroundColor="green";
+            }
+            break;
+        case "1":
+            for (let i = 0; i <=2 ; i++) {
+                let ID = "X"+i+i;
+                document.getElementById(ID).style.backgroundColor="green";
+            }
+            break;
+        case "2":
+            for (let i = 0; i <=2 ; i++) {
+                let ID = "X"+i+(2-i);
+                document.getElementById(ID).style.backgroundColor="green";
+            }
+            break;
+    }
+}
+
+function inputMark(r,c) {
+    inputPlayer(r,c);
+    if (gameType !== "1") return;
+    if (countStep <=8){
+        computerMove();
+    }
+}
+
+function computerMove() {
+    if (tableArray[1][1] === "") {
+        rPossibleWin = 1;
+        cPossibleWin = 1;
+    } else if (tableArray[1][1] === "X" && countStep <2){
+        rPossibleWin = 0;
+        cPossibleWin = 2;
+    } else {
+        checkPossibleWin();
+        checkPossibleLose();
+        if (rPossibleWin === -1 && rPossibleLose === -1){
+            checkSpecial()
         }
+    }
+    if (rPossibleWin === -1 && rPossibleLose === -1){
+        do {
+            rPossibleWin = Math.floor(Math.random()*3);
+            cPossibleWin = Math.floor(Math.random()*3);
+        } while (checkInputAlready(rPossibleWin,cPossibleWin));
+    }
+    if (rPossibleWin !== -1){
+        inputPlayer(rPossibleWin,cPossibleWin);
+        resetPossible();
+    } else {
+        inputPlayer(rPossibleLose,cPossibleLose);
+        resetPossible();
     }
 
 }
-function FillColor() {
-    if (rwin !== "") {
-        for (let i = 1; i <= 3; i++) {
-            let ID = "Y" + (rwin + 1) + i;
-            document.getElementById(ID).style.backgroundColor = "red";
+function resetPossible() {
+    rPossibleWin = -1;
+    cPossibleWin = -1;
+    rPossibleLose = -1;
+    cPossibleLose = -1;
+}
+function checkPossibleWin() {
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (tableArray[i][j] === "") {
+                tableArray[i][j] = "O";
+                if (checkWinner(i, j)) {
+                    rPossibleWin = i;
+                    cPossibleWin = j;
+                }
+                tableArray[i][j] = "";
+            }
         }
-    } else if (cwin !== "") {
-        for (let i = 1; i <= 3; i++) {
-            let ID = "Y" + i + (cwin + 1);
-            document.getElementById(ID).style.backgroundColor = "red";
+    }
+}
+function checkPossibleLose() {
+    for (let i = 0; i <= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+            if (tableArray[i][j] === "") {
+                tableArray[i][j] = "X";
+                if (checkWinner(i, j)) {
+                    rPossibleLose = i;
+                    cPossibleLose = j;
+                }
+                tableArray[i][j] = "";
+            }
         }
-    } else if (crosswin1 !==""){
-        document.getElementById("Y11").style.backgroundColor = "red";
-        document.getElementById("Y22").style.backgroundColor = "red";
-        document.getElementById("Y33").style.backgroundColor = "red";
-    } else {
-        document.getElementById("Y13").style.backgroundColor = "red";
-        document.getElementById("Y22").style.backgroundColor = "red";
-        document.getElementById("Y31").style.backgroundColor = "red";
+    }
+}
+function checkSpecial() {
+    if (tableArray[0][1] === "X" && tableArray[1][0] === "X" && !checkInputAlready(0,0)) {
+        rPossibleWin = 0;
+        cPossibleWin = 0;
+    }
+    if (tableArray[0][1] === "X" && tableArray[1][2] === "X" && !checkInputAlready(0,2)) {
+        rPossibleWin = 0;
+        cPossibleWin = 2;
+    }
+    if (tableArray[2][1] === "X" && tableArray[1][2] === "X" && !checkInputAlready(2,2)) {
+        rPossibleWin = 2;
+        cPossibleWin = 2;
+    }
+    if (tableArray[2][1] === "X" && tableArray[1][0] === "X" && !checkInputAlready(2,0)) {
+            rPossibleWin = 2;
+            cPossibleWin = 0;
     }
 }
